d238e9fd53e099de7d08ef471080e80276273fe4
Yan Daffa Putra Liandhie <144084303+YanDaffaPutraLiandhie@users.noreply.github.com>
2023-11-01 23:13:02 +0700

    penambahan dan pengubahan program

--- Perkenalan.java
+++ Perkenalan.java
@@ -1,12 +1,16 @@
 public class Perkenalan {
     public static void main(String[] args) {
         // Nama
-        System.out.println("Raffy Jamil Octavialdy");
+        System.out.println("Yan Daffa Putra Liandhie");
 
         // Kelas
-        System.out.println("2H");
+        System.out.println("1D");
 
         // Harapan
-        System.out.println("Sukses");
+        System.out.println("Menjadi web developer");
+
+        //jokes
+        System.out.println("pernah ga sih kepikiran gimana godzila reproduksi");
+
     }
 }
